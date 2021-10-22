d4a9e49325fafcdfff9ab560dcea2693d348e565
Steph Heins <stephanie_heins@yahoo.com>
2021-10-21 21:28:28 -0700

    Added user questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,68 @@
-// TODO: Include packages needed for this application
+// Packages and const
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const generateReadme = require("utils/generateMarkdown.js");
+const writeFileAsync = util.promisify(fs.writeFile);
+
+// User questions
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of this project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please provide a description of your project (what, why, how?):"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What are the steps required to install your project?"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What are some instructions for usage?"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Please choose a license for this project:",
+        choices: [
+            "Apache",
+            "Academic",
+            "GNU",
+            "MIT",
+            "GPL",
+            "Open",
+            "None"
+        ]
+    },
+    {
+        type: "input",
+        name: "contributors",
+        message: "Who are the contributors to this project?"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "How would a user perform tests for this project?"
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "What is your Github username?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?"
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
